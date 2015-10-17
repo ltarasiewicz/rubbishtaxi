ab7488bc5d0289b1aa1b38a6ae19dc5fa490cb1d
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-10-17 20:19:45 +0200

    Add a modal contace form on get a quote button on HP

---
+++ themes/rubbish_taxi/partials/quote-modal.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="quoteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Get a free quote</h4>
+            </div>
+            <div class="modal-body">
+                <?php echo do_shortcode( '[contact-form-7 id="605" title="Contact form 1"]' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
